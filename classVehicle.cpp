7dd4b8c8ca6e45c319281a1979fa50d8a53047db
Heghine Aghajanyan <50370257+heghine1302@users.noreply.github.com>
2019-06-21 02:00:27 +0400

    Update classVehicle.cpp

--- classVehicle.cpp
+++ classVehicle.cpp
@@ -92,3 +92,151 @@ int main() {
 	return 0;
 }
 
+----------------------------------------------------------------------------------------------------------------------------
+	
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Human
+{
+public:
+	string name;
+	string sex;
+	int age;
+
+	Human()
+	{
+		cout << "Constructor of Human" << endl;
+		name = "Heghine";
+		sex = "F";
+		age = 19;
+	}
+
+	Human(string name, string sex, int age)
+	{
+		this->name = name;
+		this->sex = sex;
+		this->age = age;
+	}
+
+	void HumanPrint() 
+	{
+		cout << "Name ---- " << name << "\nSex ---- " << sex << "\nAge ---- " << age << endl<<endl;
+	}
+	void cinHuman() 
+	{
+		cout << "Name --> ";
+		cin >> name;
+		cout << "Sex --> ";
+		cin >> sex;
+		cout << "Age --> ";
+		cin >> age;
+
+		cout << endl;
+	}
+	~Human() {
+		cout << "Destructor of Human" << endl;
+		system("pause");
+	}
+};
+
+class Student : virtual public Human
+{
+private:
+	int group;
+	int ID;
+
+public:
+	Student()
+	{
+		cout << "Constructor of Student" << endl;
+		group = 807;
+		ID = 1302;
+	}
+
+	Student(int group, int ID) : Human(name, sex, age)
+	{
+		this->group = group;
+		this->ID = ID;
+	}
+
+	void Learn() 
+	{
+		cout << "I'm a student" << endl;
+	}
+
+	void HumanPrint() 
+	{
+		cout << "Group ---- " << group << "\nID ---- " << ID << endl;
+		Human::HumanPrint();
+	}
+
+	void cinHuman() 
+	{
+		cout << "Group --> ";
+		cin >> group;
+		cout << "ID --> ";
+		cin >> ID;
+
+		Human::cinHuman();
+	}
+	~Student() {
+		cout << "Destructor of Student" << endl;
+		system("pause");
+	}
+};
+
+class Professor : virtual public Human
+{
+private:
+	string subject;
+
+public:
+	Professor()
+	{
+		cout << "Constructor of Professor" << endl;
+		subject = "Math";
+	}
+
+	Professor(string subject) : Human(name, sex, age)
+	{
+		this->subject = subject;
+	}
+	
+	void HumanPrint()
+	{
+		cout << "Subject ---- " << subject<<endl;
+		Human::HumanPrint();
+	}
+
+	void cinHuman() {
+		cout << "Subject --> ";
+		cin >> subject;
+
+		Human::cinHuman();
+	}
+	~Professor() {
+		cout << "Destructor of Professor" << endl;
+		system("pause");
+	}
+};
+
+int main() {
+
+	Student s;
+	s.Learn();
+	s.cinHuman();
+	s.HumanPrint();
+
+	Professor p;
+	p.cinHuman();
+	p.HumanPrint();
+
+	Human h;
+	h.cinHuman();
+	h.HumanPrint();
+	
+	system("pause");
+	return 0;
+}
